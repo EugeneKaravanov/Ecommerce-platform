67a5e5de8f21ff5d7382b7cf2136b483cdcfa2d1
EugeneKaravanov <karavanoveugene@gmail.com>
2024-12-26 20:49:25 +0300

    Commit #1.1 (Реализация модели Shard и ShardConnectionFactory)

--- Ecommerce/OrderService/Models/Shard.cs
+++ Ecommerce/OrderService/Models/Shard.cs
@@ -2,5 +2,14 @@
 {
     public class Shard
     {
+        public int BucketsCount => bucketsIds.Count;
+
+        public Shard(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public string ConnectionString { get; private set; }
+        public List<int> bucketsIds { get; set; }
     }
 }
--- Ecommerce/OrderService/Utilities/ShardConnectionFactory.cs
+++ Ecommerce/OrderService/Utilities/ShardConnectionFactory.cs
@@ -1,6 +1,61 @@
-﻿namespace OrderService.Utilities
+﻿using Npgsql;
+using OrderService.Models;
+
+namespace OrderService.Utilities
 {
     public class ShardConnectionFactory
     {
+        private List<Shard> _shards;
+        private int _bucketsCount;
+
+        public ShardConnectionFactory(List<Shard> shards)
+        {
+            _shards = shards;
+            _bucketsCount = GetBucketsCount();
+        }
+
+        public NpgsqlConnection GetConnectionByOrderId(int orderId, out string bucketName)
+        {
+            int bucketId = GetBucketNumberByOrderId(orderId);
+            string connectionString = GetShardByBucketId(bucketId).ConnectionString;
+
+            bucketName = "bucket-" + bucketId;
+            
+            return new NpgsqlConnection(connectionString);
+        }
+
+        private int GetBucketNumberByOrderId(int orderId)
+        {
+            return orderId % _bucketsCount;
+        }
+
+        private int GetBucketsCount()
+        {
+            int count = 0;
+
+            foreach (Shard shard in _shards)
+                count += shard.BucketsCount;
+
+            return count;     
+        }
+
+        private Shard GetShardByBucketId(int bucketId)
+        {
+            Shard outputShard = null;
+
+            foreach (Shard shard in _shards)
+            {
+                if (shard.bucketsIds.Contains(bucketId))
+                {
+                    outputShard = shard;
+                    return outputShard;
+                }
+            }
+
+            if (outputShard == null)
+                throw new IndexOutOfRangeException();
+
+            return outputShard;
+        }
     }
 }
