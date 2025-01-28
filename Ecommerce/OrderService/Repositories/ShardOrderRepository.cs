7c64575efb738926d7361f121519a150eb0f12b9
EugeneKaravanov <karavanoveugene@gmail.com>
2025-01-28 18:37:22 +0300

    Исправления

--- Ecommerce/OrderService/Repositories/ShardOrderRepository.cs
+++ Ecommerce/OrderService/Repositories/ShardOrderRepository.cs
@@ -86,7 +86,7 @@ namespace OrderService.Repositories
                 catch
                 {
                     await transaction.RollbackAsync();
-                    Log.Logger.Error("При выполнении метода CreateOrderAsync не удалось завершить транзакцию записи в БД!");
+                    //Здесь когда-нибудь должен появиться логгер
 
                     throw;
                 }
