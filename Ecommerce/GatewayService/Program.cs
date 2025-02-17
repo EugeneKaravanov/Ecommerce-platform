4dee9bdfcb27817472dae048b4f840d6baaf4436
EugeneKaravanov <karavanoveugene@gmail.com>
2025-02-17 11:10:05 +0300

    #5 Реализация логирования

--- Ecommerce/GatewayService/Program.cs
+++ Ecommerce/GatewayService/Program.cs
@@ -13,6 +13,12 @@ builder.Services.AddControllers(options =>
 {
     options.Filters.Add<CustomHeaderFilter>();
 });
+builder.Services.AddLogging(logging =>
+{
+    logging.ClearProviders();
+    logging.AddConsole();
+    logging.AddDebug();
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
